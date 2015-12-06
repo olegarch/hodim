8b45e80c60965036e0cabfbc75b2df0fb92c5ca2
oleg.k <oleg.k@samsung.com>
2015-12-06 02:12:10 -0800

    duplicate detection based on guid in db

--- avito/avito/pipelines.py
+++ avito/avito/pipelines.py
@@ -10,7 +10,12 @@ import json
 from scrapy.exceptions import DropItem
 from twisted.enterprise import adbapi
 import hashlib
-from twisted.internet import threads 
+from twisted.internet import threads
+
+def get_guid(item):
+    """Generates an unique identifier for a given item."""
+    # hash based solely in the url field
+    return hashlib.md5(item['url']).hexdigest()
 
 class AvitoPipeline(object):
     def process_item(self, item, spider):
@@ -25,7 +30,59 @@ class PrintPipeline(object):
             #spider.logger.debug('%s:%s',name,val.encode('utf-8') if isinstance(val, unicode) else val)
             spider.logger.debug("%s%s:%s",name,type(val),val)
         spider.logger.debug("==== PrintPipeline ====")
-        return item        
+        return item
+
+class DuplicatesPipeline(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        dbargs = dict(
+            host=settings['MYSQL_HOST'],
+            db=settings['MYSQL_DBNAME'],
+            user=settings['MYSQL_USER'],
+            passwd=settings['MYSQL_PASSWD'],
+            charset='utf8',
+            use_unicode=True,
+        )
+        dbpool = adbapi.ConnectionPool('MySQLdb', **dbargs)
+        return cls(dbpool)
+        
+    def process_item(self, item, spider):
+        # run db query in the thread pool
+        d = self.dbpool.runInteraction(self._detect_dup, item, spider)
+        d.addErrback(self._handle_error, item, spider)
+        # at the end return the item in case of success
+        d.addCallback(lambda _: item)
+        # return the deferred instead the item. This makes the engine to
+        # process next item (according to CONCURRENT_ITEMS setting) after this
+        # operation (deferred) has finished.
+        return d
+
+    def _detect_dup(self, conn, item, spider):        
+        guid = get_guid(item)
+
+        conn.execute("""SELECT EXISTS(
+            SELECT 1 FROM realestate WHERE guid = %s
+        )""", (guid, ))
+        ret = conn.fetchone()[0]
+
+        if ret:
+            raise DropItem("Duplicate item found: %s" % guid)
+        else:
+            return item
+
+    def _handle_error(self, failure, item, spider):
+        """Handle occurred on db interaction."""
+        # do nothing, just log  
+         
+        if not failure.check(DropItem):
+            print 'DuplicatesPipeline fail',failure
+
+        failure.raiseException()
+
+
 
 class GeoPipeline(object):
     def __init__(self):
@@ -98,7 +155,8 @@ class MySQLStorePipeline(object):
         d = self.dbpool.runInteraction(self._do_upsert, item, spider)
         d.addErrback(self._handle_error, item, spider)
         # at the end return the item in case of success or failure
-        d.addBoth(lambda _: item)
+        #d.addBoth(lambda _: item)
+        d.addCallback(lambda _: item)
         # return the deferred instead the item. This makes the engine to
         # process next item (according to CONCURRENT_ITEMS setting) after this
         # operation (deferred) has finished.
@@ -108,7 +166,7 @@ class MySQLStorePipeline(object):
         print '_do_upsert'+repr(dict(item)).decode("unicode-escape").encode('utf-8') + "\n"
         
         """Perform an insert or update."""
-        guid = self._get_guid(item)
+        guid = get_guid(item)
         #now = datetime.utcnow().replace(microsecond=0).isoformat(' ')
 
         conn.execute("""SELECT EXISTS(
@@ -124,8 +182,6 @@ class MySQLStorePipeline(object):
             # """, (item['name'], item['description'], item['url'], now, guid))
             print("Item updated in db: %s" % (guid))
         else:
-            loc = "POINT(%s,%s)" % (item['lon'],item['lat']) if 'lat' in item else None
-            print "LOCATION",loc
             conn.execute("""INSERT INTO realestate 
                 (guid, url, description, rooms, floor, totfloors, m2, kitchenm2, restm2, price, city, district, street, updated, location, wc, walls, ceilings, rennovation, builtdate, heating, balcony, water, security, postDate)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, POINT(%s, %s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
@@ -165,10 +221,8 @@ class MySQLStorePipeline(object):
     def _handle_error(self, failure, item, spider):
         """Handle occurred on db interaction."""
         # do nothing, just log  
-        print 'fail',failure
+        # print "we got an exception: %s" % (failure.getTraceback(),)
+        print 'MySQLStorePipeline fail',failure
         raise
 
-    def _get_guid(self, item):
-        """Generates an unique identifier for a given item."""
-        # hash based solely in the url field
-        return hashlib.md5(item['url']).hexdigest()
+
