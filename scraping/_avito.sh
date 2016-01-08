8dfd11716a5b4d86af4f10eb83922c8e0f481638
oleg <ubuntu@ip-172-31-0-94.eu-central-1.compute.internal>
2016-01-08 23:28:47 +0000

    _avito.sh updates

--- scraping/_avito.sh
+++ scraping/_avito.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/hodim/scraping
 PATH=$PATH:/usr/local/bin
 export PATH
 now=$(date +"%m_%d_%Y_%H_%M_%S")
-scrapy crawl avito -s DEPTH_LIMIT=2 -o avito.jl 2>&1 | tee avito_$now.log
+scrapy crawl avito -s DEPTH_LIMIT=20000 -o avito.jl 2>&1 | tee avito_$now.log
