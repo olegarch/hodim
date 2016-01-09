a4aa6e873aea509b32e36a1991594c37a34f3113
oleg <ubuntu@ip-172-31-0-94.eu-central-1.compute.internal>
2016-01-09 04:22:26 +0000

    crawl script

--- scraping/_crawl.sh
+++ scraping/_crawl.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/hodim/scraping
 PATH=$PATH:/usr/local/bin
 export PATH
 now=$(date +"%m_%d_%Y_%H_%M_%S")
-scrapy crawl avito -s DEPTH_LIMIT=20000 -o avito.jl 2>&1 | tee avito_$now.log
+scrapy crawl $1 -s DEPTH_LIMIT=20000 -o $1.jl 2>&1 | tee $1_$now.log
